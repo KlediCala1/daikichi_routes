ef65d4bde979ddc847134133b9ab27ff456c9740
KlediCala1 <kledicala1@gmail.com>
2023-06-13 20:25:58 +0200

    Initial commit

---
+++ src/test/java/com/dojo/daikichi_routes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.daikichi_routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
